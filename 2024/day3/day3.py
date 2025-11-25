30280e663e48ad0172187291a881a7104af7742b
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-11-25 19:16:32 +0100

    parte dos

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -11,5 +11,22 @@ def calcular_mull(memoria):
             total += int(x) * int(y)
         return total
         
+def calcular_mull_condicional(memoria):
+    with open(memoria, 'r') as file:
+        data= file.read()
+        pattern = r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)"
+        matches = re.finditer(pattern, data)
+        total = 0
+        enabled = True
+        for m in matches:
+            if m.group(0) == "do()":
+                enabled = True
+            elif m.group(0) == "don't()":
+                enabled = False
+            else:
+                x,y = m.groups()
+                total += int(x) * int(y) if enabled else 0
+        return total
 
 print("Total de la suma es:", calcular_mull(df))
+print("Total condicional de la suma es:", calcular_mull_condicional(df))
