dc844293a69d6052c7f74a54069ba1cc36b77aad
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-11-25 18:39:27 +0100

    parte 2 completa

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 
 data_file = 'lista.csv'
 
@@ -16,7 +16,21 @@ def Distancia(data_file):
     distancia_total = sum(abs(a-b) for a,b in zip(fila_izq, fila_der))
     return distancia_total
 
-
+def Similaridad(data_file):
+    fila_izq = []
+    fila_der = []
+    # Abre el archivo CSV y separa valores en las dos listas
+    with open(data_file, mode='r',) as csvfile:
+        for row in csvfile:
+            a, b = map(int, row.split('   '))
+            fila_izq.append(a)
+            fila_der.append(b)
+            
+            contador_der = Counter(fila_der)
+            similitud_total = sum(a * contador_der[a] for a in fila_izq)
+    return similitud_total
+    
         
 print(Distancia(data_file))
+print(Similaridad(data_file))
 # Llamar a la función para llenar las listas antes de imprimir
